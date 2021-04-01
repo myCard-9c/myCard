e701a689a0d14f7ae52cfdcd00e35720f445ef8c
Alex Kilkenny <2464370k@student.gla.ac.uk>
2021-04-01 20:43:39 +0100

    Card model functional

--- myCard/admin.py
+++ myCard/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from myCard.models import Card
 
-# Register your models here.
+
+admin.site.register(Card)
--- myCard/models.py
+++ myCard/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.core.validators import validate_email, MaxValueValidator,URLValidator
 # Create your models here.
+
+
+class Card(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    first_name = models.CharField(max_length=100,blank = True)
+    last_name = models.CharField(max_length=100,blank = True)
+    email =  models.EmailField(max_length=150,blank = True)
+    phone_no =  models.BigIntegerField(validators = [MaxValueValidator(9999999999)],default = 0000000000)
+    location =  models.CharField(max_length=150,blank = True)
+    occupation =  models.CharField(max_length=50,blank = True)
+    website =  models.URLField(blank = True)
+    picture = models.ImageField(blank = True)
+    media_links =  models.URLField(blank = True)
+    about =  models.CharField(blank = True,max_length=150)
+    visibility =  models.BooleanField()
+    def __str__(self):
+        return self.name
