f1761b97a1b33b946888fc0260bab7f432da078e
Manuel Simonetta <2472780S@student.gla.ac.uk>
2021-04-04 13:46:02 +0200

    Solved register page layout

--- myCard/forms.py
+++ myCard/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from myCard.models import UserProfile
+from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
 class UserForm(forms.ModelForm):
@@ -8,3 +8,27 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'password',)
+
+class NewUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "first_name", "last_name", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=True)
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        if commit:
+            user.save()
+        return user
+
+    def __init__(self, *args, **kwargs):
+        super(NewUserForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ["username", "first_name", "last_name", "email", "password1", "password2"]:
+            self.fields[fieldname].help_text = None
--- myCard/views.py
+++ myCard/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from myCard.forms import UserForm
+from myCard.forms import NewUserForm
 from myCard.models import Card
 from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from myCard.models import Card
 from django.views import View
+
 def index(request):
     # List of the last 3 visible Card
     card_list = Card.objects.order_by('-id').filter(visibility=True)[:3]
@@ -50,25 +52,17 @@ def user_logout(request):
     logout(request)
     return redirect(reverse('myCard:index'))
 
-
 def register(request):
-    registered = False
-
-    if request.method == 'POST':
-        user_form = UserForm(request.POST)
-
-        if user_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
-            registered = True
-
-        else:
-            print(user_form.errors)
-    else:
-        user_form = UserForm()
-
-    return render(request, 'myCard/register.html', context = {'user_form' : user_form, 'registered' : registered})
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+			messages.success(request, "Registration successful." )
+			return redirect("myCard:index")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm
+	return render (request=request, template_name="myCard/register.html", context={"register_form":form})
 
 @login_required
 def dashboard(request):
@@ -90,11 +84,11 @@ def create_card(request):
 def resume_tips(request):
     # Return the response
     return render(request, 'myCard/resume_tips.html')
-    
-    
+
+
 def edit_card(request):
    return render(request, 'myCard/edit_card.html')
-   
+
 class generate_card(View):
     def get(self, request):
         card = Card.objects.order_by('id')[0]
